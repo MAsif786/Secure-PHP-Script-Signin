7b11a5535c33ffc12928361a836ac8d113c263bf
Willy Fox <BlackVikingPro@users.noreply.github.com>
2016-11-27 20:58:20 -0600

    Update one-page.php
    Added the "password_hash()" function for better security.

--- one-page.php
+++ one-page.php
@@ -1,62 +1,77 @@
 <?php
-$username = "my-user";
-$password = "my-pass";
-$nonsense = "supercalifragilisticexpialidocious";
 
-if (isset($_COOKIE['PrivatePageLogin'])) {
-   if ($_COOKIE['PrivatePageLogin'] == md5($password.$nonsense)) {
+$password = "password123"; // Password.
+
+(!isset($_SESSION) ? session_start() : $uselessvar = true);
+
+/*
+  ___                   ____
+ / _ \ _ __   ___      |  _ \ __ _  __ _  ___
+| | | | '_ \ / _ \_____| |_) / _` |/ _` |/ _ \
+| |_| | | | |  __/_____|  __/ (_| | (_| |  __/
+ \___/|_| |_|\___|     |_|   \__,_|\__, |\___|
+                                   |___/
+ ____
+| __ ) _   _ _
+|  _ \| | | (_)
+| |_) | |_| |_
+|____/ \__, (_)
+       |___/
+ ____  _            _   __     ___ _    _             ____
+| __ )| | __ _  ___| | _\ \   / (_) | _(_)_ __   __ _|  _ \ _ __ ___
+|  _ \| |/ _` |/ __| |/ /\ \ / /| | |/ / | '_ \ / _` | |_) | '__/ _ \
+| |_) | | (_| | (__|   <  \ V / | |   <| | | | | (_| |  __/| | | (_) |
+|____/|_|\__,_|\___|_|\_\  \_/  |_|_|\_\_|_| |_|\__, |_|   |_|  \___/
+                                                |___/
+*/
+
+if (isset($_POST['logout'])) {
+	session_unset();
+	session_destroy();
+	echo "Logged out successful.";
+} 
+
+if (isset($_SESSION['token'])) {
+	if (password_verify($password, $_SESSION['token'])) {
 ?>
 
-    <!-- LOGGED IN CONTENT HERE -->
-    <!DOCTYPE html>
-    <html>
-    <head>
-      <title>Successful Login</title>
-      <link rel="icon" type="image/x-icon" href="https://blackvikingpro.com/images/favicon.ico">
-    </head>
-    <body>
-
-      <p>Successful Login...</p>
-    
-    </body>
-    </html>
+<!-- User is logged in. -->
+<!DOCTYPE html>
+<html>
+<head>
+	<title>One Page | Signed In</title>
+</head>
+<body><p>Signed In</p><form action="" method="post"><button type="submit" name="logout">Logout</button></form>
+</body>
+</html>
 
 <?php
-      exit;
-   } else {
-      echo "Bad Cookie.";
-      exit;
-   }
+		die();
+	}
 }
 
-if (isset($_GET['p']) && $_GET['p'] == "login") {
-   if ($_POST['user'] != $username) {
-      echo "Sorry, that username does not match. <br /> <a href='index.php'> <--Go Back. </a>";
-      exit;
-   } else if ($_POST['keypass'] != $password) {
-      echo "Sorry, that password does not match. <br /> <a href='index.php'> <--Go Back. </a>";
-      exit;
-   } else if ($_POST['user'] == $username && $_POST['keypass'] == $password) {
-      setcookie('PrivatePageLogin', md5($_POST['keypass'].$nonsense));
-      header("Location: $_SERVER[PHP_SELF]");
-   } else {
-      echo "Sorry, you could not be logged in at this time.";
-   }
+if (isset($_POST['password'])) {
+	$pass = $_POST['password'];
+	if ($pass == $password) {
+		$_SESSION['token'] = password_hash($password, PASSWORD_DEFAULT); // not the best encryption/hashing, but it get's the job done.
+		$self = $_SERVER['PHP_SELF'];
+		header("Location: $self");
+	} else {
+		echo " Incorrect Password. ";
+	}
 }
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-   <title>Secure Login Script Using PHP</title>
-   <link rel="icon" type="image/x-icon" href="https://blackvikingpro.com/images/favicon.ico">
+	<title>Login!</title>
 </head>
 <body>
 
-<form action="<?php echo $_SERVER['PHP_SELF']; ?>?p=login" method="post">
-<label><input type="text" name="user" id="user" /> User</label><br />
-<label><input type="password" name="keypass" id="keypass" /> Password</label><br />
-<input type="submit" id="submit" value="Login" />
+<form action="" method="post">
+	<label>Password: </label><input type="text" name="password" autofocus="" />
+	<button type="submit">Submit</button>
 </form>
 
 </body>
